e9ece3553a5d190da81e443616d0f3660f049577
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-06-07 13:27:57 +0300

    commit

--- DOM Manipulations and Events Exercise/06. Furniture/solution.js
+++ DOM Manipulations and Events Exercise/06. Furniture/solution.js
@@ -7,41 +7,44 @@ function solve() {
   buyBtn.addEventListener('click', buy);
   let tableBody = document.querySelector('tbody');
 
-  function generate(){
-    let furnitureArr = JSON.parse(input[0].value);
-    for (const obj of furnitureArr) {
-      let row = document.createElement('tr');
-      for (const key in obj) {
-        let cell = document.createElement('td');
-        if (key == 'img') {
-          let imgElement = document.createElement('img');
-          imgElement.src = obj[key];
-          cell.appendChild(imgElement);
-        }
-        cell.textContent = obj[key];
-        row.appendChild(cell);
+  function generate() {
+      let furnitureArr = JSON.parse(input[0].value);
+      for (const obj of furnitureArr) {
+          let row = document.createElement('tr');
+          for (const key in obj) {
+              let cell = document.createElement('td');
+              if (key == 'img') {
+                  let imgElement = document.createElement('img');
+                  imgElement.src = obj.img;
+                  cell.appendChild(imgElement);
+                  row.appendChild(cell);
+
+              } else {
+                  cell.textContent = obj[key];
+                  row.appendChild(cell);
+              }
+              
+          }
+          let chkBox = document.createElement('input');
+          chkBox.setAttribute('type', 'checkbox');
+          row.appendChild(chkBox);
+          tableBody.appendChild(row);
       }
-      let chkBox = document.createElement('input');
-      chkBox.setAttribute('type','checkbox');
-      row.appendChild(chkBox);
-      tableBody.appendChild(row);
-    }
   }
 
-  function buy(){
-    let totalPrice = 0;
-    let totalFactor = 0;
-    let names = [];
-    let rows = Array.from(document.getElementsByTagName('tr')).filter(e=> e.children[4].checked == true);
-    for (const row of rows) {
-      names.push(row.children[1].textContent);
-      totalPrice += Number(row.children[2].textContent);
-      totalFactor += Number(row.children[3].textContent);
-    }
-    input[1].value += `Bought furniture: ${names.join(', ')}.\n`;
-    input[1].value += `Total price: ${totalPrice.toFixed(2)}\n`;
-    input[1].value +=  `Average decoration factor: ${totalFactor / rows.length}`;
-    
-  }
+  function buy() {
+      let totalPrice = 0;
+      let totalFactor = 0;
+      let names = [];
+      let rows = Array.from(document.getElementsByTagName('tr')).filter(e => e.children[4].checked == true);
+      for (const row of rows) {
+          names.push(row.children[1].textContent);
+          totalPrice += Number(row.children[2].textContent);
+          totalFactor += Number(row.children[3].textContent);
+      }
+      input[1].value += `Bought furniture: ${names.join(', ')}.\n`;
+      input[1].value += `Total price: ${totalPrice.toFixed(2)}\n`;
+      input[1].value += `Average decoration factor: ${totalFactor / rows.length}`;
 
+  }
 }
--- DOM Manipulations and Events Exercise/09. JavaScript-Quiz/solution.js
+++ DOM Manipulations and Events Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,49 @@
 function solve() {
-  //TODO...
+  let sections = document.querySelectorAll('section');
+  let resultElement = document.querySelector('.results-inner');
+  let firstSection = sections[0];
+  let secondSection = sections[1];
+  let thirdSection = sections[2];
+  let buttonsFirstSection = Array.from(firstSection.querySelectorAll('ul li p'));
+  let buttonsSecondSetion = Array.from(secondSection.querySelectorAll('ul li p'));
+  let buttonsThirdSection = Array.from(thirdSection.querySelectorAll('ul li p'));
+  buttonsFirstSection.forEach(b=>b.addEventListener('click', onClickFirst));
+  buttonsSecondSetion.forEach(b=>b.addEventListener('click', onClickSecond));
+  buttonsThirdSection.forEach(b=>b.addEventListener('click', onClickThird));
+  let points = 0;
+
+
+  function onClickFirst(event){
+    let currentBtn = event.target;
+    if (currentBtn.textContent == 'onclick') {
+      points++;
+    }
+    firstSection.style.display = 'none';
+    secondSection.style.display = 'block';
+
+  }
+  function onClickSecond(event){
+    let currentBtn = event.target;
+    if (currentBtn.textContent == 'JSON.stringify()') {
+      points++;
+    }
+    secondSection.style.display = 'none';
+    thirdSection.style.display = 'block';
+  }
+
+  function onClickThird(event){
+    let currentBtn = event.target;
+    if (currentBtn.textContent == 'A programming API for HTML and XML documents') {
+      points++;
+    }
+    thirdSection.style.display = 'none';
+    resultElement.parentNode.style.display = 'block';
+    if (points == 3) {
+      resultElement.children[0].textContent = 'You are recognized as top JavaScript fan!';  
+    } else {
+      resultElement.children[0].textContent = `You have ${points} right answers`;
+    }
+    
+  }
+  
 }
